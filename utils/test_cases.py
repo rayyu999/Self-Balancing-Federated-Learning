3fd25f77daa7840358b7e1eaf789963d9ffaadfc
rayyu999 <411205547@qq.com>
2021-12-09 16:09:24 +0800

    add size imbalance test case

--- utils/test_cases.py
+++ utils/test_cases.py
@@ -4,7 +4,10 @@
 # size_imbalance_list = [20000, 7000, 3000]
 
 # 6 users & 3 mediators
-size_imbalance_list = [10000, 10000, 5000, 3000, 1000, 1000]
+# size_imbalance_list = [10000, 10000, 5000, 3000, 1000, 1000]
+
+# 9 users & 3 mediators
+size_imbalance_list = [7000, 5500, 5000, 5000, 3000, 2000, 1000, 1000, 500]
 
 # size_imbalance_list = [
 #                         5000, 5000, 5000, 5000, 5000, 5500, 1000, 1000, 1000, 1000,
